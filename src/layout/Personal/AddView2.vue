c7de09281b095805235102a21f60fbf97a6583c5
xiaona yang <xiaona.yang@quanbio.com>
2022-05-31 10:04:40 +0800

    5.31morning1

--- src/layout/Personal/AddView2.vue
+++ src/layout/Personal/AddView2.vue
@@ -1,21 +1,51 @@
 <template>
-  <div>
-
+  <div class="qus">
+    <p>Subscription depends on:</p>
+    <p>- number of patients ?</p>
+    <p>- number of doctors ?</p>
+    <p>- duration</p>
+    <p>- .. ?</p>
+    <p>****************</p>
+    <p>Subscription Description:</p>
+    <p>- date of purchase</p>
+    <p>- duration</p>
+    <p>- number max of patients per plan</p>
+    <p>- number max doctors per plan</p>
+    <p>- list of people using the subscription</p>
+    <p>- Who pay it: the doctor , the hospital admin or patient ?</p>
+    <p>- will send invoice to customers ?</p>
+    <p>Does it have a trial time ?</p>
+    <p>What are the different possible plans ?</p>
+    <p>What are the payment methods? Is it Per month or year ?</p>
+    <!-- <p>
+      Subscription Description:
+      <br />- date of purchase
+      <br />- duration
+      <br />- number max of patients per plan
+      <br />- number max doctors per plan
+      <br />- list of people using the subscription
+      <br />- Who pay it: the doctor , the hospital admin or patient ?
+      <br />- will send invoice to customers ?
+      <br />Does it have a trial time ?
+      <br />What are the different possible plans ?
+      <br />What are the payment methods? Is it Per month or year ?
+    </p> -->
   </div>
 </template>
 
 <script>
 export default {
-
   data() {
     return {};
   },
-  created(){},
-  mounted(){},
-  methods:{},
+  created() {},
+  mounted() {},
+  methods: {}
 };
 </script>
 
 <style  scoped>
-
+.qus {
+  background-color: white;
+}
 </style>
