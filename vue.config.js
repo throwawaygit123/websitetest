e464c0b7e45595c5f60346783136129559b2e3ac
xiaona yang <xiaona.yang@quanbio.com>
2022-07-22 09:59:55 +0800

    7.22morning1

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-    host: '192.168.3.9',
-    // https:true,
-    port: 8080,
-    client: {
-      webSocketURL: 'wss://192.168.3.9:8080',
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    }
-  },
+  
   transpileDependencies: true
+  
 })
